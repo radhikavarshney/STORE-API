3c449e020339df538e73c224a2d3b140cb0f750a
radhikavarshney <rvarshney2001@gmail.com>
2022-12-17 21:39:23 +0530

    2md commit

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+require('dotenv').config()
+
+const exp = require('express')
+const app = exp()
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+//invoking express
+app.use(exp.json())
+
+//routes
+
+app.get(('/'), (req,res)=>{
+    res.send('<h1>hella yeah its working bitch </h1><a href="/api/v1/product">Products Route</a>')
+})
+app.use('/api/v1/products')
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+const start= async()=>{
+
+    await connectDB(process.env.MONGO_URI)
+    try {
+        app.listen(port, console.log(`user hit the server ${port} `))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
+
+
