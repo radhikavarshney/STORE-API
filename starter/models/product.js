1bbfc133d066ed6bace77bbbffd87d0e55c495e6
radhikavarshney <rvarshney2001@gmail.com>
2022-12-19 15:01:43 +0530

    8th commit

--- starter/models/product.js
+++ starter/models/product.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose')
 
-const productSchema = new mongoose.Schema({})
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, 'products name must be provided']
+    },
+    price:{
+        type: Number,
+        required: [true,'products price must be provided']
+    },
+    featured:{
+        type:Boolean,
+        default: false
+    },
+    rating:{
+     type:Number,
+     default:4.5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
