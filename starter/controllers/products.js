d56ae65ee788745423d03985111fe8b22bd13350
radhikavarshney <rvarshney2001@gmail.com>
2022-12-20 19:00:29 +0530

    11th commit

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -4,17 +4,25 @@ const Product = require('../models/product')
 const getAllProductsStatic = async (req,res)=>{
     // throw new Error('testing async error')
     const products = await Product.find({
-        name: 'vase table'
+        name: 'albany sectional'
     })
     res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured}= req.query
+    const {featured,company,name}= req.query
     const queryObject= {}
+    if(company){
+        queryObject.company = company 
+
+    }
+    if(name){
+        queryObject.name = name 
+
+    }
 if(featured){
     queryObject.featured = featured === 'true' ? true :false
-}
+}  
 console.log(queryObject);
     const products = await Product.find(queryObject)
     res.status(200).json({products, nbHits: products.length})
--- starter/populate.js
+++ starter/populate.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const connectDB = require("./db/connect")
-const Products= require('./models/product')
+// const connectDB = require("./db/connect")
+// const Products= require('./models/product')
 
-const jsonProduct = require('./products.json')
+// const jsonProduct = require('./products.json')
 
 
-const start = async ()=>{
-    try{
-        await connectDB(process.env.MONGO_URI)
-        await Products.deleteMany()
-        await Products.create(jsonProduct)
-        console.log("success!!!!");
-        process.exit(0)
-    }catch(error){
-        console.log(error)
-        process.exit(1)
-    }
-}
+// const start = async ()=>{
+//     try{
+//         await connectDB(process.env.MONGO_URI)
+//         await Products.deleteMany()
+//         await Products.create(jsonProduct)
+//         console.log("success!!!!");
+//         process.exit(0)
+//     }catch(error){
+//         console.log(error)
+//         process.exit(1)
+//     }
+// }
 
-start()
+// start()
