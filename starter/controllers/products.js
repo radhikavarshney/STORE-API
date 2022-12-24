1785a5961046da71d817b79c12a4efc1d689c91b
radhikavarshney <rvarshney2001@gmail.com>
2022-12-24 22:55:29 +0530

    13th commit

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -3,28 +3,44 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res)=>{
     // throw new Error('testing async error')
+    const search = 'a'
     const products = await Product.find({
-        name: 'albany sectional'
-    })
+        // name: { $regex: search , $options :'i'}
+        //i is for denoting case insensitive
+    }).select('name price')
+    // .sort('-name price')
     res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured,company,name}= req.query
+    const {featured,company,name,sort}= req.query
     const queryObject= {}
     if(company){
         queryObject.company = company 
 
     }
     if(name){
-        queryObject.name = name 
+        queryObject.name = { $regex: name , $options :'i'}
 
     }
     if(featured){
     queryObject.featured = featured === 'true' ? true :false
 }  
-console.log(queryObject);
-    const products = await Product.find(queryObject)
+// console.log(queryObject);
+    let result =Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result =result.sort(sortList)
+    }
+    else{
+        result =result.sort('createAt')
+    }
+
+    if(fields){
+            const fieldsList = fields.split(',').join(' ')
+            result =result.select(fieldsList)
+        }
+    const products = await result
     res.status(200).json({products, nbHits: products.length})
 } 
 
