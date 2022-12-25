ec80d244c248338dc9adc37743b4c9781d045803
radhikavarshney <rvarshney2001@gmail.com>
2022-12-25 23:16:53 +0530

    14th commit

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -7,13 +7,13 @@ const getAllProductsStatic = async (req,res)=>{
     const products = await Product.find({
         // name: { $regex: search , $options :'i'}
         //i is for denoting case insensitive
-    }).select('name price')
+    }).select('name price').limit(21)
     // .sort('-name price')
     res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured,company,name,sort}= req.query
+    const {featured,company,name,sort,fields}= req.query
     const queryObject= {}
     if(company){
         queryObject.company = company 
@@ -33,7 +33,7 @@ const getAllProducts = async (req,res)=>{
         result =result.sort(sortList)
     }
     else{
-        result =result.sort('createAt')
+        result =result.sort('createdAt')
     }
 
     if(fields){
